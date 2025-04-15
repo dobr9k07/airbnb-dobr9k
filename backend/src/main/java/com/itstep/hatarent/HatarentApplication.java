6a59c0fd595e81e721cb874012f42981142dc825
mr_void <47479465+mr-V01d@users.noreply.github.com>
2025-04-15 10:06:03 +0300

    Рефакторинг: зміна структури проєкту, розділення на backend і frontend

---
+++ backend/src/main/java/com/itstep/hatarent/HatarentApplication.java
@@ -0,0 +1,13 @@
+package com.itstep.hatarent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HatarentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HatarentApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/itstep/hatarent/HatarentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itstep.hatarent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HatarentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
