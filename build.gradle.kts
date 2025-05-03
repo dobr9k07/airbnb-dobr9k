d0dae3cd4ede4ddbdc9f3a51eb1c3c22c1b7cfdb
mr_void <botnebotua@gmail.com>
2025-05-03 18:10:03 +0300

    chore: додав необхідні залежності
    Додав інтерективне документування RESTful api - Swagger, також
    зафіксував версії усіх залежностей

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,17 +19,25 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-security")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.flywaydb:flyway-core")
-	implementation("org.flywaydb:flyway-mysql")
-	//developmentOnly("org.springframework.boot:spring-boot-docker-compose")
-	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
-	providedRuntime("org.springframework.boot:spring-boot-starter-tomcat")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.security:spring-security-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.4.5")
+	implementation("org.springframework.boot:spring-boot-starter-security:3.4.5")
+	implementation("org.springframework.boot:spring-boot-starter-web:3.4.5")
+
+	implementation("org.flywaydb:flyway-core:11.8.0")
+	implementation("org.flywaydb:flyway-mysql:11.8.0")
+
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.6")
+
+	runtimeOnly("org.mariadb.jdbc:mariadb-java-client:3.5.3")
+
+	providedRuntime("org.springframework.boot:spring-boot-starter-tomcat:3.4.5")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test:3.4.5")
+	testImplementation("org.springframework.security:spring-security-test:6.4.5")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.13.0-M2")
+
+  compileOnly("org.projectlombok:lombok:1.18.38")
+  annotationProcessor("org.projectlombok:lombok:1.18.38")
 }
 
 tasks.withType<Test> {
