ee12dacb68e8d0339b9c43662f375d40d93f6a2d
mr_void <botnebotua@gmail.com>
2025-05-05 16:58:10 +0300

    fix(DB): виправлення точності позицій і цін

---
+++ src/main/resources/db/migration/V4__alter_rental_location.sql
@@ -0,0 +1,2 @@
+ALTER TABLE hatarent.rentals CHANGE longtitude longitude decimal(20,20) NOT NULL;
+ALTER TABLE hatarent.rentals MODIFY COLUMN latitude decimal(20,20) NOT NULL;
---
+++ src/main/resources/db/migration/V5__alter_price_precision.sql
@@ -0,0 +1,1 @@
+ALTER TABLE hatarent.rentals MODIFY COLUMN price_per_night decimal(19,4) NOT NULL;
