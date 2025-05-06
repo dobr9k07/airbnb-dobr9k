6251d36e1a6c4810a1374ede68634b5b0670e059
mr_void <botnebotua@gmail.com>
2025-05-06 09:21:14 +0300

    fix(DB): виплавлення точності позиції

---
+++ src/main/java/com/itstep/hatarent/util/SqlSet.java
@@ -0,0 +1,4 @@
+package com.itstep.hatarent.util;
+
+public class SqlSet {
+}
---
+++ src/main/resources/db/migration/V6__alter_location_precision.sql
@@ -0,0 +1,2 @@
+ALTER TABLE hatarent.rentals MODIFY COLUMN longitude decimal(9,6) NOT NULL;
+ALTER TABLE hatarent.rentals MODIFY COLUMN latitude decimal(9,6) NOT NULL;
