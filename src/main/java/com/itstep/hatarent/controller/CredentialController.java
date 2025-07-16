b5c955729a36dec19756c23f85811ad652116a44
mr_void <47479465+mr-V01d@users.noreply.github.com>
2025-07-16 10:55:05 +0300

    feat(API): додав валідацію до CredentialController.java

--- src/main/java/com/itstep/hatarent/controller/CredentialController.java
+++ src/main/java/com/itstep/hatarent/controller/CredentialController.java
@@ -5,19 +5,20 @@ import com.itstep.hatarent.model.User;
 import com.itstep.hatarent.service.HatarentUserDetailsService;
 import com.itstep.hatarent.service.UserService;
 import com.itstep.hatarent.util.JwtUtil;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.media.Content;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -49,7 +50,7 @@ public class CredentialController {
           @ApiResponse(responseCode = "409", description = "Пользователь с такой почтой уже сувществует", content = @Content),
       })
   @PostMapping("register")
-  public ResponseEntity<String> register(@RequestBody AuthenticationRequest auhtenticationRequest) {
+  public ResponseEntity<String> register(@Valid @RequestBody AuthenticationRequest auhtenticationRequest) {
     if (userService.isEmailTaken(auhtenticationRequest.getEmail())) {
       return ResponseEntity.status(HttpStatus.CONFLICT).build();
     }
@@ -72,7 +73,7 @@ public class CredentialController {
           @ApiResponse(responseCode = "403", description = "Неправильные данные авторизации", content = @Content),
       })
   @PostMapping("/login")
-  public ResponseEntity<String> loginUser(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
+  public ResponseEntity<String> loginUser(@Valid @RequestBody AuthenticationRequest authenticationRequest) throws Exception {
     return ResponseEntity.ok(generateToken(authenticationRequest));
   }
 
--- src/main/java/com/itstep/hatarent/dto/credential/AuthenticationRequest.java
+++ src/main/java/com/itstep/hatarent/dto/credential/AuthenticationRequest.java
@@ -1,11 +1,18 @@
 package com.itstep.hatarent.dto.credential;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @AllArgsConstructor
 @Data
 public class AuthenticationRequest {
+  @Schema(description = "Почта пользователя")
+  @Email
   private String email;
+  @Schema(description = "Пароль пользователя")
+  @Size(min = 6, max = 20)
   private String password;
 }
