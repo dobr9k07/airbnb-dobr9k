849beb1ef266254c069015231b7bbb17a8cd022a
mr_void <47479465+mr-V01d@users.noreply.github.com>
2025-07-16 10:56:07 +0300

    feat(API): покращив обробку помилок у GlobalExceptionHandler.java

--- src/main/java/com/itstep/hatarent/controller/GlobalExceptionHandler.java
+++ src/main/java/com/itstep/hatarent/controller/GlobalExceptionHandler.java
@@ -1,33 +1,62 @@
 package com.itstep.hatarent.controller;
 
-import org.apache.coyote.BadRequestException;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
 import java.nio.file.NoSuchFileException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.NoSuchElementException;
+
+import org.apache.coyote.BadRequestException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
- @ExceptionHandler(NoSuchElementException.class)
- public ResponseEntity<String> noSuchElementException(NoSuchElementException ex) {
-   return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
- }
 
- @ExceptionHandler(NoSuchFileException.class)
- public ResponseEntity<String> noSuchFileException(NoSuchFileException ex) {
-   return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
- }
+  private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
- @ExceptionHandler(BadRequestException.class)
- public ResponseEntity<String> wrongRequestException(Exception ex) {
-   return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
- }
+  @ResponseStatus(HttpStatus.NOT_FOUND)
+  @ExceptionHandler({NoSuchElementException.class, NoSuchFileException.class})
+  public String handleNotFoundExceptions(Exception ex) {
+    return ex.getMessage();
+  }
 
- @ExceptionHandler(Exception.class)
- public ResponseEntity<String> genericException(Exception ex) {
-   return new ResponseEntity<>("Server error", HttpStatus.INTERNAL_SERVER_ERROR);
- }
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  @ExceptionHandler({BadRequestException.class, HttpMessageNotReadableException.class})
+  public String handleBadRequest(final RuntimeException ex) {
+    return ex.getMessage();
+  }
+
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public Map<String, String> handleValidationExceptions(
+    MethodArgumentNotValidException ex) {
+      Map<String, String> errors = new HashMap<>();
+      ex.getBindingResult().getAllErrors().forEach((error) -> {
+          String fieldName = ((FieldError) error).getField();
+          String errorMessage = error.getDefaultMessage();
+          errors.put(fieldName, errorMessage);
+      });
+      return errors;
+  }
+
+  @ResponseStatus(HttpStatus.FORBIDDEN)
+  @ExceptionHandler(AccessDeniedException.class)
+  public String handleAccessDenied(final AccessDeniedException ex) {
+    return ex.getMessage();
+  }
+
+  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+  @ExceptionHandler(RuntimeException.class)
+  public String handleGenericRuntimeException(final RuntimeException ex) {
+    logger.error("Unhandled server error: ", ex);
+    return "Server error";
+  }
 }
